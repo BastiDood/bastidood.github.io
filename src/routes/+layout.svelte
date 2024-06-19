df379f5f38c377641770616acab83a7d4ccbc46c
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2024-06-20 03:52:12 +0800

    refactor: move static files to library

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,14 @@
     import './index.css';
     import Footer from './Footer.svelte';
     import Header from './Header.svelte';
+    import favicon from '$lib/favicon.png?url';
+    import og from '$lib/og.webp?url';
 </script>
+
+<svelte:head>
+    <link rel="icon" href={favicon} />
+    <meta property="og:image" content="https://bastidood.github.io{og}" />
+</svelte:head>
 
 <Header />
 <main class="space-y-8 bg-gradient-to-b from-sky to-white">
