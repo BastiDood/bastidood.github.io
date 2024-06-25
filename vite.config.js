6e48a9715b4e3b35409b457239e2cc56973629ef
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2024-06-25 14:43:11 +0800

    chore: revert `.postcssrc.json` config

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,6 @@
+import autoprefixer from 'autoprefixer';
+import tailwindcss from 'tailwindcss';
+
 import { defineConfig } from 'vite';
 import { purgeCss } from 'vite-plugin-tailwind-purgecss';
 import { sveltekit } from '@sveltejs/kit/vite';
@@ -5,4 +8,5 @@ import { sveltekit } from '@sveltejs/kit/vite';
 export default defineConfig({
     plugins: [sveltekit(), purgeCss()],
     build: { assetsInlineLimit: 0 },
+    css: { postcss: { plugins: [tailwindcss, autoprefixer] } },
 });
