b4e4267981f0a82bae38137f47ee6b87e8730678
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2024-09-14 02:09:31 +0800

    feat: include profile picture in hero section

--- src/routes/Hero.svelte
+++ src/routes/Hero.svelte
@@ -2,6 +2,7 @@
     import dev from '$lib/icons/social/dev.svg';
     import github from '$lib/icons/social/github.svg';
     import linkedIn from '$lib/icons/social/linkedin.svg';
+    import profile from '$lib/images/profile.webp';
     import twitter from '$lib/icons/social/twitter.svg';
 </script>
 
@@ -11,6 +12,13 @@
         <p>Web developer. Open-source contributor. Learner. Writer. Mentor. Leader.</p>
     </div>
     <div class="flex items-center gap-2">
+        <img
+            src={profile}
+            width="500"
+            height="500"
+            alt="cartoonish avatar waving hello"
+            class="block size-8 rounded-full"
+        />
         <a href="https://github.com/BastiDood" title="GitHub"><img src={github} alt="GitHub" class="w-8" /></a>
         <a href="https://dev.to/somedood" title="dev.to"><img src={dev} alt="dev.to" class="w-8" /></a>
         <a href="https://www.linkedin.com/in/basti-ortiz/" title="LinkedIn"
