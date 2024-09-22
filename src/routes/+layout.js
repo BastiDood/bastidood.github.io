461a106329d381eb284db803d553303db35a0977
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2024-09-23 04:19:04 +0800

    fix: disable analytics for non-production environments

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,11 +1,11 @@
-import { browser } from '$app/environment';
+import { browser, building, dev } from '$app/environment';
 import { posthog } from 'posthog-js';
 
 export const prerender = true;
 export const trailingSlash = 'always';
 
 export function load() {
-    if (browser)
+    if (browser && !building && !dev)
         posthog.init('phc_f0e5e0c7IsAgDbqtkDfHGlBPQmW2Sn5KIaTQ7hMSEzF', {
             api_host: 'https://us.i.posthog.com',
             person_profiles: 'never',
