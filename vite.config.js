48c27e6bad9f45a9363927b0a09ad4bab349ae2c
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2025-03-04 03:05:29 +0800

    deps!: migrate to Tailwind 4

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,9 @@
-import autoprefixer from 'autoprefixer';
-import tailwindcss from 'tailwindcss';
-
 import { defineConfig } from 'vite';
-import { purgeCss } from 'vite-plugin-tailwind-purgecss';
 import { sveltekit } from '@sveltejs/kit/vite';
 
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineConfig({
-    plugins: [sveltekit(), purgeCss()],
+    plugins: [sveltekit(), tailwindcss()],
     build: { assetsInlineLimit: 0 },
-    css: { postcss: { plugins: [tailwindcss, autoprefixer] } },
 });
