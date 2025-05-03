cc15f01a24ef9d5122285e762aee1b6e843b8756
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2025-05-03 13:55:00 +0800

    chore: remove minifier

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,11 @@
 /** @import { Config } from '@sveltejs/kit' */
 
-import minifyAdapter from 'sveltekit-html-minifier';
-import staticAdapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-static';
 
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {Config} */
 export default {
   preprocess: vitePreprocess(),
-  kit: minifyAdapter({ adapter: staticAdapter({ fallback: '404.html' }) }),
+  kit: { adapter: adapter({ fallback: '404.html' }) },
 };
