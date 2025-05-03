b0435767115d03adab356763fccefb237db1731b
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2025-05-03 13:47:30 +0800

    feat: use `sveltekit-html-minifier

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,12 @@
 /** @import { Config } from '@sveltejs/kit' */
 
-import adapter from '@sveltejs/adapter-static';
+import minifyAdapter from 'sveltekit-html-minifier';
+import staticAdapter from '@sveltejs/adapter-static';
+
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {Config} */
 export default {
     preprocess: vitePreprocess(),
-    kit: { adapter: adapter({ fallback: '404.html' }) },
+    kit: minifyAdapter({ adapter: staticAdapter({ fallback: '404.html' }) }),
 };
