91227a518f1dbd60a301c8a15fa02c73952b3a48
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2025-05-24 16:33:04 +0800

    feat(ui): add card component

---
+++ src/lib/ui/card.svelte
@@ -0,0 +1,34 @@
+<script module lang="ts">
+  import { type VariantProps, cva } from 'class-variance-authority';
+
+  const card = cva([], {
+    variants: {
+      color: {
+        card: 'bg-card',
+        fade: 'bg-fade',
+      },
+      size: {
+        sm: 'rounded-lg p-4 shadow-md',
+        md: 'rounded-xl p-8 shadow-xl',
+        lg: 'rounded-2xl p-12 shadow-2xl',
+      },
+    },
+    defaultVariants: {
+      color: 'card',
+    },
+  });
+
+  type CardProps = VariantProps<typeof card>;
+</script>
+
+<script lang="ts">
+  import type { HTMLAttributes } from 'svelte/elements';
+
+  import { twMerge } from 'tailwind-merge';
+
+  type Props = CardProps & HTMLAttributes<HTMLDivElement>;
+  const { color, size, class: className, children }: Props = $props();
+  const classes = $derived(twMerge(card({ color, size, className })));
+</script>
+
+<div class={classes}>{@render children?.()}</div>
