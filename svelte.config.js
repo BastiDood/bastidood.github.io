2aba3d2f371cde3efc576b06c14b0afb7569afa6
Basti Ortiz <ortiz@bastidood.dev>
2025-07-19 09:26:01 +0800

    chore: simplify `svelte.config.js`

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,5 @@
 /** @import { Config } from '@sveltejs/kit' */
-
 import adapter from '@sveltejs/adapter-static';
 
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-
 /** @type {Config} */
-export default {
-  preprocess: vitePreprocess(),
-  kit: { adapter: adapter({ fallback: '404.html' }) },
-};
+export default { kit: { adapter: adapter({ fallback: '404.html' }) } };
