c616aae04294ab72fc33d0a054f7852ff1b6c7ba
Basti Ortiz <ortiz@bastidood.dev>
2025-08-09 15:40:47 +0800

    refactor: prefer sync parser for `valibot`

--- src/routes/articles/api.js
+++ src/routes/articles/api.js
@@ -1,11 +1,12 @@
-import { arrayAsync, parseAsync } from 'valibot';
+import * as v from 'valibot';
 
 import { Article } from './model';
 
+const Articles = v.array(Article);
 export async function fetchArticles() {
   // https://developers.forem.com/api#version-1
   const res = await fetch('https://dev.to/api/articles?username=somedood&state=all&per_page=6', {
     headers: { Accept: 'application/vnd.forem.api-v1+json' },
   });
-  return parseAsync(arrayAsync(Article), await res.json());
+  return v.parse(Articles, await res.json());
 }
