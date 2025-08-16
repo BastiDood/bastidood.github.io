3938db7b0d6625842c30884addd6b888bcd56a15
Basti Ortiz <ortiz@bastidood.dev>
2025-08-17 02:51:00 +0800

    fix: ensure service worker waits for skip

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -10,13 +10,13 @@ import { assert } from './lib/assert';
 /** @see https://kit.svelte.dev/docs/service-workers */
 const sw = self as unknown as ServiceWorkerGlobalScope;
 
-async function addFilesToCache() {
+async function onInstall() {
   const cache = await caches.open(version);
-  await cache.addAll(build.concat(files).concat(prerendered));
-  sw.skipWaiting();
+  await cache.addAll([...build, ...files, ...prerendered]);
+  await sw.skipWaiting();
 }
 
-async function deleteOldCache() {
+async function onActivate() {
   const keys = new Set(await caches.keys());
   // eslint-disable-next-line no-console
   if (keys.delete(version)) console.log(`deleting existing at ${version}`);
@@ -29,12 +29,12 @@ async function deleteOldCache() {
   );
 }
 
-async function fetchCacheFirst(req: Request) {
+async function onFetch(req: Request) {
   const cache = await caches.open(version);
   const res = await cache.match(req);
   return res ?? fetch(req);
 }
 
-sw.addEventListener('install', evt => evt.waitUntil(addFilesToCache()));
-sw.addEventListener('activate', evt => evt.waitUntil(deleteOldCache()));
-sw.addEventListener('fetch', evt => evt.respondWith(fetchCacheFirst(evt.request)));
+sw.addEventListener('install', evt => evt.waitUntil(onInstall()));
+sw.addEventListener('activate', evt => evt.waitUntil(onActivate()));
+sw.addEventListener('fetch', evt => evt.respondWith(onFetch(evt.request)));
