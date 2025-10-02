07cf022300858a99fe1db8ceaf3c87c32c4a21a1
Basti Ortiz <ortiz@bastidood.dev>
2025-10-02 08:36:48 +0800

    refactor: use environment variables for Posthog API key

---
+++ src/lib/env.js
@@ -0,0 +1,3 @@
+import { env } from '$env/dynamic/public';
+
+export const { PUBLIC_POSTHOG_API_KEY } = env;
--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -5,12 +5,17 @@ import { browser, building, dev } from '$app/environment';
 export const prerender = true;
 export const trailingSlash = 'always';
 
-export function load() {
-  if (browser && !building && !dev)
-    posthog.init('phc_f0e5e0c7IsAgDbqtkDfHGlBPQmW2Sn5KIaTQ7hMSEzF', {
-      api_host: 'https://us.i.posthog.com',
-      person_profiles: 'never',
-      capture_pageview: true,
-      capture_pageleave: 'if_capture_pageview',
-    });
+export async function load() {
+  if (browser && !building && !dev) {
+    const { PUBLIC_POSTHOG_API_KEY } = await import('$lib/env');
+    if (typeof PUBLIC_POSTHOG_API_KEY === 'undefined')
+      console.warn('PUBLIC_POSTHOG_API_KEY is absent... skipping PostHog initialization');
+    else
+      posthog.init(PUBLIC_POSTHOG_API_KEY, {
+        api_host: 'https://us.i.posthog.com',
+        person_profiles: 'never',
+        capture_pageview: true,
+        capture_pageleave: 'if_capture_pageview',
+      });
+  }
 }
