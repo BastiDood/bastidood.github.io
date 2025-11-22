ec5e619f3c3e480042f8afdd85183136396c7098
Basti Ortiz <ortiz@bastidood.dev>
2025-11-22 20:48:00 +0800

    feat(ui): add timeline component

---
+++ src/lib/components/career-timeline/card.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+
+	import Card from '$lib/ui/card.svelte';
+
+	interface Props {
+		date: string;
+		title: string;
+		companyName: string;
+		companyUrl: string;
+		children?: Snippet;
+	}
+
+	const { date, title, companyName, companyUrl, children }: Props = $props();
+</script>
+
+<div
+	class="group relative flex max-w-6xl items-center justify-between md:justify-normal md:odd:flex-row-reverse"
+>
+	<!-- Dot -->
+	<div
+		class="absolute left-0 flex size-10 shrink-0 translate-x-0 items-center justify-center rounded-full md:static md:order-1 md:translate-x-0"
+	>
+		<div class="size-3 rounded-full bg-[#b2ebff]"></div>
+	</div>
+	<!-- Content -->
+	<div class="ml-12 w-[calc(100%-4rem)] md:ml-0 md:w-[calc(50%-2.5rem)]">
+		<Card size="sm" class=" bg-white/90 backdrop-blur-sm transition-colors hover:bg-white">
+			<time class="mb-1 block text-xs text-slate-500">{date}</time>
+			<h3 class="text-2xl font-bold text-slate-800">{title}</h3>
+			<div class="mb-2 text-sm font-medium text-sky-600">
+				<a href={companyUrl} target="_blank" rel="external" class="hover:underline"
+					>{companyName} ↗</a
+				>
+			</div>
+			{#if children}
+				<div class="text-sm text-slate-600">
+					{@render children()}
+				</div>
+			{/if}
+		</Card>
+	</div>
+</div>
---
+++ src/lib/components/career-timeline/root.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+	import { twMerge } from 'tailwind-merge';
+
+	interface Props {
+		class?: string;
+		children: Snippet;
+	}
+
+	const { children, class: className }: Props = $props();
+	const classes = $derived(
+		twMerge(
+			'relative space-y-8 before:absolute before:inset-0 before:ml-5 before:h-full before:w-0.5 before:-translate-x-px before:bg-linear-to-b before:from-transparent before:via-slate-300 before:to-transparent md:before:mx-auto md:before:translate-x-0',
+			className,
+		),
+	);
+</script>
+
+<div class={classes}>
+	{@render children()}
+</div>
