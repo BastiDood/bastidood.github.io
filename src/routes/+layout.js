86cc8a7d33cf656cf8807dbc7c34950851905579
Basti Ortiz <ortiz@bastidood.dev>
2026-01-24 10:43:32 +0800

    chore: use more defaults from PostHog

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -12,12 +12,7 @@ export function load() {
 	if (browser && !building && !dev)
 		if (PUBLIC_POSTHOG_API_KEY) {
 			console.log('PUBLIC_POSTHOG_API_KEY is present... initializing PostHog');
-			posthog.init(PUBLIC_POSTHOG_API_KEY, {
-				api_host: 'https://us.i.posthog.com',
-				person_profiles: 'never',
-				capture_pageview: true,
-				capture_pageleave: 'if_capture_pageview',
-			});
+			posthog.init(PUBLIC_POSTHOG_API_KEY, { person_profiles: 'never' });
 		} else {
 			console.warn('PUBLIC_POSTHOG_API_KEY is absent... skipping PostHog initialization');
 		}
